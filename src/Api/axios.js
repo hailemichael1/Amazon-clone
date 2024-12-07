f9683faa8dac7384bb6482f2b8d0eb8f1f5cae5b
hayleuser <hailyemisganew@gmail.com>
2024-12-07 14:09:22 -0900

    Backend deployment baseURL

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- 
-  baseURL: "http://127.0.0.1:5001/clone-253c3/us-central1/api",
+  //local instance of firebase functions
+  // baseURL: "http://localhost:3679/clone-253c3/us-central1/api",
+
+  // deployed version of firebase function 
+  // baseURL: "http://127.0.0.1:5001/clone-253c3/us-central1/api",
+
+  // deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-4tbk.onrender.com/",
 });
 
 export { axiosInstance };
